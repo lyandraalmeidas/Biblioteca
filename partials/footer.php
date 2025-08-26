80b8d19ae018e848dc75c9076ff0224d0ad38144
haiagos <hiagodonascimentosilva02@gmail.com>
2025-08-26 22:45:04 +0000

    ajustes 2

--- partials/footer.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Footer parcial
-?>
-<footer class="border-top py-3 mt-auto">
-  <div class="container">
-    <p class="text-center mb-0">&copy; <?php echo date('Y'); ?> Biblioteca Pessoal</p>
-  </div>
-</footer>
